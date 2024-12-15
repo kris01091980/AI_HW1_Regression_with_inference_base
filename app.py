258ef8d765d9be8e35410766c27419248ca5cfc2
kris01091980 <59123649+kris01091980@users.noreply.github.com>
2024-12-15 14:59:06 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,21 +1,33 @@
-from io import StringIO
 from typing import List
-
-import joblib
-import pandas as pd
 from fastapi import FastAPI, File, UploadFile
 from pydantic import BaseModel
+import pandas as pd
+from io import StringIO
+import pickle
+import warnings
 
+from starlette.responses import JSONResponse
+
+warnings.filterwarnings('ignore')
+
+# Создаем приложение FastAPI
 app = FastAPI()
 
-model = joblib.load("model.pkl")
-scaler = joblib.load("scaler.pkl")
+# Загружаем данные из одного .pickle файла
+with open("models.pickle", "rb") as f:
+    saved_data = pickle.load(f)
+
+model = saved_data["linear_model"]  # Используем модель Ridge
+scaler = saved_data["scaler"]  # Загружаем StandardScaler
+feature_names = saved_data["feature_names"]  # Список признаков
+label_encoder = saved_data["label_encoder"]  # LabelEncoder для 'name'
+one_hot_columns = saved_data["one_hot_columns"]  # OneHot-кодированные столбцы
 
 
+# Описание базового объекта с помощью Pydantic
 class Item(BaseModel):
     name: str
     year: int
-    selling_price: int
     km_driven: int
     fuel: str
     seller_type: str
@@ -28,36 +40,96 @@ class Item(BaseModel):
     seats: float
 
 
+# Описание коллекции объектов
 class Items(BaseModel):
     objects: List[Item]
 
 
+# Метод для обработки одного объекта
 @app.post("/predict_item")
 def predict_item(item: Item) -> float:
-    data = pd.DataFrame([item.dict()])
-    data = pd.get_dummies(data, columns=['fuel', 'seller_type', 'transmission', 'owner'], drop_first=True)
-    data_scaled = scaler.transform(data)
-    prediction = model.predict(data_scaled)
+    # Преобразование объекта в DataFrame
+    data = preprocess(pd.DataFrame([item.dict()]))
+
+    numeric_columns = ['year', 'km_driven', 'mileage', 'engine', 'max_power',
+                       'seats']
+    df_train_numeric = data[numeric_columns]
+
+    # Предсказание
+    prediction = model.predict(df_train_numeric)
     return float(prediction[0])
 
 
+# Метод для обработки коллекции объектов
 @app.post("/predict_items")
 def predict_items(items: Items) -> List[float]:
+    # Преобразование объектов в DataFrame
     data = pd.DataFrame([item.dict() for item in items.objects])
-    data = pd.get_dummies(data, columns=['fuel', 'seller_type', 'transmission', 'owner'], drop_first=True)
-    data_scaled = scaler.transform(data)
-    predictions = model.predict(data_scaled)
+
+    # Предобработка данных
+    data = preprocess(data)
+
+    numeric_columns = ['year', 'km_driven', 'mileage', 'engine', 'max_power',
+                       'seats']
+    df_train_numeric = data[numeric_columns]
+
+    # Предсказание
+    predictions = model.predict(df_train_numeric)
     return predictions.tolist()
 
 
+# Метод для обработки .csv файла
 @app.post("/predict_csv")
-async def predict_csv(file: UploadFile = File(...)) -> str:
+async def predict_csv(file: UploadFile = File(...)) -> JSONResponse:
+    # Чтение содержимого файла
     contents = await file.read()
-    df = pd.read_csv(StringIO(contents.decode('utf-8')))
-    df = pd.get_dummies(df, columns=['fuel', 'seller_type', 'transmission', 'owner'], drop_first=True)
-    df_scaled = scaler.transform(df)
-    predictions = model.predict(df_scaled)
-    df['predicted_price'] = predictions
+
+    # Создание DataFrame из CSV
+    df = pd.read_csv(StringIO(contents.decode("utf-8")))
+
+    # Предобработка данных
+    df = preprocess(df)
+
+    numeric_columns = ['year', 'km_driven', 'mileage', 'engine', 'max_power',
+                       'seats']
+    df_train_numeric = df[numeric_columns]
+
+    # Предсказание
+    predictions = model.predict(df_train_numeric)
+    df["predicted_price"] = predictions
+
+    # Сохранение результата в новый CSV файл
     output_file = "predicted_" + file.filename
     df.to_csv(output_file, index=False)
     return {"filename": output_file}
+
+
+def preprocess(data):
+    data = data.dropna()
+
+    # Убедимся, что столбцы являются строковыми перед заменой
+    data['mileage'] = data['mileage'].astype(str).str.replace(r' kmpl| km/kg', '', regex=True)
+    data['mileage'] = pd.to_numeric(data['mileage'], errors='coerce')
+
+    data['engine'] = data['engine'].astype(str).str.replace(' CC', '')
+    data['engine'] = pd.to_numeric(data['engine'], errors='coerce')
+
+    data['max_power'] = data['max_power'].astype(str).str.replace(' bhp', '')
+    data['max_power'] = pd.to_numeric(data['max_power'], errors='coerce')
+
+    # Удаляем столбец 'torque'
+    data = data.drop(columns=['torque'])
+
+    # Преобразуем столбец engine в числовой тип, заменяя все недопустимые значения на NaN
+    data['engine'] = pd.to_numeric(data['engine'], errors='coerce')
+
+    # Если нужно, можно заполнить NaN значениями (например, средним или медианным значением)
+    data['engine'].fillna(data['engine'].mean(), inplace=True)
+
+    # Преобразуем в целочисленный тип
+    data['engine'] = data['engine'].astype(int)
+
+    # Преобразуем столбец seats в целочисленный тип
+    data['seats'] = data['seats'].fillna(0).astype(int)
+
+    return data
